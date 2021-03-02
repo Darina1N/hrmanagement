e6fa8db4bd536c678c8dbb62876f88f01c4dd76e
Darina Kolesarova <darina.kolesarova@kosickaakademia.sk>
2021-03-02 10:49:43 +0100

    trieda User, Enum, nastavenie

--- src/main/java/sk/kosickaakademia/company/entity/User.java
+++ src/main/java/sk/kosickaakademia/company/entity/User.java
@@ -1,26 +1,49 @@
 package sk.kosickaakademia.company.entity;
 
+import sk.kosickaakademia.company.enumerator.Gender;
+
 public class User {
     private int id;
     private String fname;
     private String lname;
     private int age;
-    private boolean gender;
+    private Gender gender;
 
-    public User(int id, String fname, String lname, int age, boolean gender) {
+    public User(int id, String fname, String lname, int age, int gender) {
+        this(fname, lname, age, gender); // volám druhý konštruktor, musí byť prvý ešte pred zadaním zvyšných parametrov tohto konštruktora
         this.id = id;
-        this.fname = fname;
-        this.lname = lname;
-        this.age = age;
-        this.gender = gender;
     }
 
-    public User(String fname, String lname, int age, boolean gender) {
+    public User(String fname, String lname, int age, int gender) {
         this.fname = fname;
         this.lname = lname;
         this.age = age;
-        this.gender = gender;
+        this.gender = gender==0?Gender.Male : gender==1?Gender.Female : Gender.Other;
+        //terárny operátor ktorý mi pri hodnote 0 dá M, hodnote 1 dá F a pri akomkoľvek inom čísle dá Other
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Gender getGender() {
+        return gender;
     }
 }
+
+
+
 
 
